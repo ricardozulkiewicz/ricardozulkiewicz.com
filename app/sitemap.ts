a2bf3bae038c1de979302fb84c8168147aa2003d
ricardozulkiewicz <ricardomachado.zulk@gmail.com>
2026-05-25 21:04:35 -0300

    Add Portuguese page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://ricardozulkiewicz.com/pt",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: "https://ricardozulkiewicz.com/cv",
       lastModified: new Date(),
       changeFrequency: "monthly",
