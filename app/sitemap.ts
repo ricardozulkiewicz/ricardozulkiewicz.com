48a5fa1522bae483c9ec09e089545c3d8c6797ad
ricardozulkiewicz <ricardomachado.zulk@gmail.com>
2026-05-25 22:12:10 -0300

    Add terms page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -26,5 +26,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.4,
     },
+    {
+      url: "https://ricardozulkiewicz.com/termos",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+    },
   ];
 }
