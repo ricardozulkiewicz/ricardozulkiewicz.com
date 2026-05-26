db2528b1e71810d5f9e1c83319cb0adc9a163f24
ricardozulkiewicz <ricardomachado.zulk@gmail.com>
2026-05-25 21:52:53 -0300

    Add privacy page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,5 +20,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: "https://ricardozulkiewicz.com/privacidade",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+    },
   ];
 }
