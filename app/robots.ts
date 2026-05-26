cde010646a110a2ef7f5fff8f790c3754442972b
ricardozulkiewicz <ricardomachado.zulk@gmail.com>
2026-05-25 21:03:58 -0300

    Block sensitive CV routes from robots

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,10 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: [
+        "/cv/access",
+        "/api/cv/",
+      ],
     },
     sitemap: "https://ricardozulkiewicz.com/sitemap.xml",
   };
